b1637f91fb813428e528d2b5a2810fe17b8a1c86
gem-abhinavpuri <122994296+gem-abhinavpuri@users.noreply.github.com>
2024-03-03 13:28:54 +0530

    a

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI = "mongodb+srv://Abhinav:gbGy07EeiwiNvhTe@cluster0.urvmp8c.mongodb.net/"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
